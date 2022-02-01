f9d2ffe29a6d30ad76e8ca307a4467a3355f2395
Simon5155Q <75984605+Simon5155Q@users.noreply.github.com>
2022-02-01 19:54:12 +0530

    Add files via upload

---
+++ c85-authentication/config.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+export const firebaseConfig = {
+  apiKey: "AIzaSyANtS_Z-Ee1msjBXdPmRYkLrbw7ta_eGV8",
+  authDomain: "class85-f8686.firebaseapp.com",
+  databaseURL: "https://class85-f8686-default-rtdb.asia-southeast1.firebasedatabase.app",
+  projectId: "class85-f8686",
+  storageBucket: "class85-f8686.appspot.com",
+  messagingSenderId: "36588082506",
+  appId: "1:36588082506:web:dbc694b70573bcc7001db3"
+};
+
+// Initialize Firebase
+
